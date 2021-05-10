793e9e64852af8e02c5e283e99d1d51074133bde
Stuart Mumford <stuart@cadair.com>
2021-05-10 11:26:23 +0100

    Actually run the export command in the script

--- export.sh
+++ export.sh
@@ -5,6 +5,7 @@ index=./index.org
 progn="(progn
   (package-initialize)
   (add-to-list 'load-path \"~/.emacs.d/elpa/28.0/develop/ox-reveal-20210411.1138/\")
-  (require 'ox-reveal))"
+  (require 'ox-reveal)
+  (org-reveal-export-to-html))"
 
 emacs --batch --visit "$index" --eval "$progn" --kill
