dba843bf17846c4745af7cc9e9cee7e94950a7e3
Stuart Mumford <stuart@cadair.com>
2021-06-28 12:03:41 +0100

    Update for working previews

--- export.sh
+++ export.sh
@@ -2,9 +2,11 @@
 
 index=./index.org
 
+oxreveal=$(find ~/.emacs.d/elpa/28.0/develop/ -type d -name "ox-reveal-*" -print -quit)
+
 progn="(progn
   (package-initialize)
-  (add-to-list 'load-path \"~/.emacs.d/elpa/28.0/develop/ox-reveal-20210411.1138/\")
+  (add-to-list 'load-path \"$oxreveal\")
   (require 'ox-reveal)
   (org-reveal-export-to-html))"
 
