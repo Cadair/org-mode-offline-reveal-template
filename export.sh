af6c31f1ac30b53b43345e3dab596d04ccd5a746
Stuart Mumford <stuart@cadair.com>
2025-06-05 09:15:51 +0100

    use local emacs packages in export.sh

--- export.sh
+++ export.sh
@@ -4,20 +4,17 @@ index=./talk.org
 
 progn="(progn
   (package-initialize)
-  (require 'use-package-ensure) ;; Load use-package-always-ensure
-  (setq package-archives '((\"melpa\" . \"https://melpa.org/packages/\")
-                           (\"org\" . \"https://orgmode.org/elpa/\")
-                           (\"elpa\" . \"https://elpa.gnu.org/packages/\")))
-
-  (use-package ox-reveal :ensure t)
-  (use-package htmlize :ensure t)
+  (use-package ox-reveal
+    :ensure t
+    :vc t
+ 	:load-path \"$(pwd)/src/emacs/org-reveal/\")
+  (use-package htmlize
+    :ensure t
+    :vc t
+    :load-path \"$(pwd)/src/emacs/htmlize/\")
   (require 'ox-reveal)
   (org-babel-do-load-languages 'org-babel-load-languages '((python . t)))
   (setq org-confirm-babel-evaluate nil)
-  ;; Set more reveal options not supported by ox-reveal
-  (defun cadair/org-reveal-scripts (scripts)
-    (concat scripts \"<script>Reveal.configure({hash:true});</script>\"))
-  (advice-add 'org-reveal-scripts :filter-return #'cadair/org-reveal-scripts)
   (org-reveal-export-to-html))"
 
 emacs --no-init-file --batch --visit "$index" --eval "$progn" --kill
